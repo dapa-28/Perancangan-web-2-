f52dc42c0bc557b31b07a6e6f1afde20980d70dc
dapa-28 <daffaramdani605@gmail.com>
2025-12-22 18:58:47 +0700

    Update proses.php

--- PRAKTIKUM 11 MPDF/codingan/proses.php
+++ PRAKTIKUM 11 MPDF/codingan/proses.php
@@ -78,12 +78,12 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'hugotegal@gmail.com';
-    $mail->Password   = 'kxrk upmw appk anog';
+    $mail->Username   = '**********';
+    $mail->Password   = '********';
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;
 
-    $mail->setFrom('hugotegal@gmail.com', 'Sistem Reminder');
+    $mail->setFrom('**********', 'Sistem Reminder');
     $mail->addAddress($email);
 
     $mail->Subject = 'Notifikasi Sistem Reminder';
@@ -101,4 +101,5 @@ try {
 } catch (Exception $e) {
     echo "Email gagal: {$mail->ErrorInfo}";
 }
+
 ?>
